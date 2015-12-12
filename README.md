07dd3a9c147d5f32782b2829db3558d199366df5
Alberto Barradas <abcsds@gmail.com>
2015-12-11 22:33:10 -0600

    Add bode diagram Use scipy to model time-invariant system

--- README.md
+++ README.md
@@ -2,6 +2,47 @@
 
 Utiliza Python 2.7. La referenca para PySide está [aquí](http://pyside.github.io/). Y ejemplos [acá](https://github.com/PySide/Examples/tree/050809faad4e8f58e89ab53df9e3f86045f98c48/examples/widgets).
 
-### Librerías:
+## Librerías
  - PySide
  - matplotlib
+
+## Fuentes
+ - [Lowpass](http://sim.okawa-denshi.jp/en/CRhikeisan.htm)
+ - [Highpass](http://sim.okawa-denshi.jp/en/LRlowkeisan.htm)
+ - [Bandpass](http://sim.okawa-denshi.jp/en/RLCbpkeisan.htm)
+ - [Bandstop](http://sim.okawa-denshi.jp/en/RLCbekeisan.htm)
+
+## ITL
+Description of a Linear time-invariant system, described by it's zeros, poles and gain. Gain is zero for all filters `20*log(1) = 0`. A resistance of 1 Ohm is considered for all filters.
+
+```
+scipy.signal.lti(zeros, poles, gain)
+```
+
+### RL
+`(R/L)/(S+(R/L))`
+
+zeros = `[]`
+poles = `[-1/L]`
+gain = `0`
+
+### CR
+`S/(S+(1/CR))`
+
+zeros = `[0]`
+poles = `[-1/C]`
+gain = `0`
+
+### LCR (Bandpass)
+`((R/L)S)/(S**2+S(R/L)+(1/LC))`
+
+zeros = `[0]`
+poles = `[((-1/L)-(1/L)*(C-4L))/2,((-1/L)+(1/L)*(C-4L))/2]`
+gain = `0`
+
+### RLC (Bandstop)
+`((s**2)+(1/LC))/(S**2+S(R/L)+(1/LC))`
+
+zeros = `[(-1/LC)**(1/2),-(-1/LC)**(1/2)]`
+poles = `[((-1/L)-(1/L)*(C-4L))/2,((-1/L)+(1/L)*(C-4L))/2]`
+gain = `0`
--- filters.py
+++ filters.py
@@ -11,9 +11,17 @@ from matplotlib.figure import Figure
 
 from PySide import QtCore, QtGui
 
+from scipy import signal
+
+
 class Window(QtGui.QWidget):
     def __init__(self):
         super(Window, self).__init__()
+        self.R = 1
+        self.L = 0
+        self.C = 0
+
+        self.sig = signal.lti([10], [50,200], 40)
 
         menuGroup = QtGui.QGroupBox("Options")
 
@@ -23,21 +31,26 @@ class Window(QtGui.QWidget):
         dropdown.addItem("Highpass")
         dropdown.addItem("Bandpass")
         dropdown.addItem("Bandstop")
+        self.filterType = 0 # Starting in Lowpass filter so, no capacitor
+
 
         inductorGroup = QtGui.QGroupBox("Inductor")
-        self.LLineEdit = QtGui.QInputDialog()
-        self.LLineEdit.setInputMode(QtGui.QInputDialog.DoubleInput)
+        self.LLineEdit = QtGui.QDoubleSpinBox()
+        self.LLineEdit.setMinimum(0)
         inductorLabel = QtGui.QLabel("Inductance:")
 
         capacitorGroup = QtGui.QGroupBox("Capacitor")
-        self.CLineEdit = QtGui.QInputDialog()
-        self.CLineEdit.setInputMode(QtGui.QInputDialog.DoubleInput)
+        self.CLineEdit = QtGui.QDoubleSpinBox()
+        self.CLineEdit.setMinimum(0)
         capacitorLabel = QtGui.QLabel("Capacitance:")
 
-        # Starting in Lowpass filter so, no capacitor
+        # No capacitor in this lowpass
         self.CLineEdit.setEnabled(False)
 
+        # Connect signals and slots
         dropdown.activated[int].connect(self.filterChanged)
+        self.LLineEdit.valueChanged [float].connect(self.indChanged)
+        self.CLineEdit.valueChanged [float].connect(self.capChanged)
 
         LLayout = QtGui.QHBoxLayout()
         LLayout.addWidget(inductorLabel,0)
@@ -50,7 +63,7 @@ class Window(QtGui.QWidget):
         capacitorGroup.setLayout(CLayout)
 
         self.circuitLabel = QtGui.QLabel(self)
-        self.circuit(0)
+        self.circuit()
 
         menuLayout = QtGui.QVBoxLayout()
         menuLayout.addWidget(filterLabel, 0)
@@ -70,6 +83,10 @@ class Window(QtGui.QWidget):
         self.setWindowTitle("Filters")
 
     def filterChanged(self, index):
+        '''
+        Self defined slot that gets called when the filter type changes
+        '''
+        self.filterType=index
         if index == 0:
             self.LLineEdit.setEnabled(True)
             self.CLineEdit.setEnabled(False)
@@ -82,26 +99,73 @@ class Window(QtGui.QWidget):
         elif index == 3:
             self.LLineEdit.setEnabled(True)
             self.CLineEdit.setEnabled(True)
-        self.circuit(index)
+        self.circuit()
+        self.setFunction()
+        self.plot()
+
+
+    def setFunction(self):
+        '''
+        Set bode function from transfer function
+        '''
+        if self.filterType == 0:
+            # -R/L
+            self.sig = signal.lti([], [-1/self.L], 0)
+        elif self.filterType == 1:
+            # -1/RC
+            self.sig = signal.lti([0], [-1/self.C], 0)
+        elif self.filterType == 2:
+            self.sig = signal.lti(\
+            [0,float("inf")], \
+            [((-1/self.L)-(1/self.L)*(self.C-4*self.L))/2,((-1/self.L)+(1/self.L)*(self.C-4*self.L))/2], \
+            0)
+        elif self.filterType == 3:
+            self.sig = signal.lti(\
+            [(-1/self.L*self.C)**(1/2),-(-1/self.L*self.C)**(1/2)], \
+            [((-1/self.L)-(1/self.L)*(self.C-4*self.L))/2,((-1/self.L)+(1/self.L)*(self.C-4*self.L))/2], \
+            40)
+
+    def indChanged(self, val):
+        '''
+        Self defined slot that gets called when the value of the inductor changes
+        '''
+        self.L = val
+        self.setFunction()
+        self.plot()
+
+    def capChanged(self, val):
+        '''
+        Self defined slot that gets called when the value of the capacitor changes
+        '''
+        self.C = val
+        self.setFunction()
+        self.plot()
 
     def plot(self):
-        # generate the plot
+        '''
+        Here goes the matplotlib magic: this function generates the Bode diagram
+        '''
         fig = Figure(figsize=(600,600), dpi=72, facecolor=(1,1,1), edgecolor=(0,0,0))
-        ax = fig.add_subplot(111)
-        ax.plot([0,1,3,2,2,3,4,2,1,2,3])
+        graph = fig.add_subplot(1,1,1,xscale="log")
+        # self.sig = signal.lti([10], [50,200], 40)
+        w, mag, phase = signal.bode(self.sig)
+        graph.plot(w, mag)
+
         # generate the canvas to display the plot
         self.canvas = FigureCanvas(fig)
 
-    def circuit(self,index):
-        if index == 0:
+    def circuit(self):
+        '''
+        Draw the circuit as a pixmap
+        '''
+        if self.filterType == 0:
             pixmap = QtGui.QPixmap("Lowpass.png")
-        elif index == 1:
+        elif self.filterType == 1:
             pixmap = QtGui.QPixmap("Hipass.png")
-        elif index == 2:
+        elif self.filterType == 2:
             pixmap = QtGui.QPixmap("Bandpass.png")
-        elif index == 3:
+        elif self.filterType == 3:
             pixmap = QtGui.QPixmap("Bandstop.png")
-
         self.circuitLabel.setPixmap(pixmap)
 
 
